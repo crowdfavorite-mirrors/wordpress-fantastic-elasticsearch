6d3a7eb4bc03e31a41c3ca776aa6dda2001d84e5
Matt Richmond <matt@crowdfavorite.com>
2013-12-12 16:39:42 -0700

    Rewriting the post status hook section, in order to handle publishing and unpublishing posts with its respective indexing/non-indexing actions

--- wp/admin/hooks.php
+++ wp/admin/hooks.php
@@ -3,28 +3,25 @@ namespace elasticsearch;
 
 class Hooks{
 	function __construct(){
-		add_action( 'save_post', array( &$this, 'save_post' ) );
+		add_action( 'transition_post_status', array( $this, 'determine_action_by_post_status' ), 10, 3 );
 		add_action( 'delete_post', array( &$this, 'delete_post' ) );
-		add_action( 'trash_post', array( &$this, 'delete_post' ) );
 	}
-	
-	function save_post( $post_id ) {
-		if(is_object( $post_id )){
-			$post = $post_id;
-		} else {
-			$post = get_post( $post_id );
-		}
 
-		if($post == null || !in_array($post->post_type, Config::types())){
+	function determine_action_by_post_status( $new_status, $old_status, $post ) {
+		if( !is_object( $post ) || !in_array( $post->post_type, Config::types() ) ) {
 			return;
 		}
 
-		if ($post->post_status == 'trash'){
-			Indexer::delete($post);
-		}
+		$indexable_statuses = Config::indexable_statuses();
+		$nonindexable_statuses = Config::nonindexable_statuses();
 
-		if ($post->post_status == 'publish'){
+		if ( in_array( $new_status, $indexable_statuses ) ) {
+			// includes updates to a post
 			Indexer::addOrUpdate($post);
+		}
+		// If we had been indexable status before, and we're now a non-indexable status remove the post
+		else if ( in_array( $new_status, $nonindexable_statuses ) && in_array( $old_status, $indexable_statuses ) ) {
+			Indexer::delete($post);
 		}
 	}
 
